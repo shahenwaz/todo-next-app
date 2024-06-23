20f1bbdc5882247be47565e2e97e44f544bfe3ba
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-06-23 05:48:11 +0100

    Dynamic Metadata

---
+++ app/blog/[post]/layout.js
@@ -0,0 +1,11 @@
+const BlogLayout = ({ children }) => {
+  return children;
+};
+
+export default BlogLayout;
+
+export async function generateMetadata({ params }) {
+  return {
+    title: params.post.split("-").join(" "),
+  };
+}
