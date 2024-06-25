43a042160ff70e61977373dc60113a8d128cb927
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-06-25 23:02:14 +0100

    useState variable created to store user input (title, description)

--- app/page.js
+++ app/page.js
@@ -1,16 +1,34 @@
+"use client";
 import Table from "@/components/Table";
+import { useState } from "react";
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData((form) => ({ ...form, [name]: value }));
+    console.log(formData);
+  };
+
   return (
     <>
       <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-16 px-2 mx-auto">
         <input
+          value={formData.title}
+          onChange={onChangeHandler}
           type="text"
           name="title"
           placeholder="Enter Title"
           className="px-3 py-2 border-2 w-full rounded"
         />
         <textarea
+          value={formData.description}
+          onChange={onChangeHandler}
           name="description"
           placeholder="Enter Description"
           className="px-3 py-2 border-2 rounded w-full"
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <div className="flex py-3 flex-wrap justify-around">
-      <h1 className="text-lg font-semibold">Todo App</h1>
+      <h1 className="text-lg font-semibold">
+        <Link href={"./"}>Todo App</Link>
+      </h1>
       <ul className="flex gap-[40px]">
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>
+          <Link href={"./"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"./products"}>Products</Link>
+        </li>
+        <li>
+          <Link href={"./about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"./contact"}>Contact</Link>
+        </li>
       </ul>
     </div>
   );
