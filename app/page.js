637de5250861ad0ae99c5a47ba06390df13848b9
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-06-25 23:22:36 +0100

    API - 'AXIOS package' & React-Toastify library added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 "use client";
 import Table from "@/components/Table";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   // Using useState hook to store the form data
@@ -20,10 +22,19 @@ export default function Home() {
   // To prevent the page from reloading when the form is submitted
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      // API code goes here to get http request
+
+      // Toast Notification
+      toast.success("Task Added Successfully");
+    } catch (error) {
+      toast.error("Error");
+    }
   };
 
   return (
     <>
+      <ToastContainer theme="dark" />
       <form
         onSubmit={onSubmitHandler}
         className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-16 px-2 mx-auto"
