dddebc360d5b65f85933e8bead4b2f1c68cb6ce9
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-06-26 02:00:40 +0100

    Database connection added - MongoDB

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,12 @@
+import { ConnectDB } from "@/lib/config/db";
 import { NextResponse } from "next/server";
+
+// Database connection
+const LoadDB = async () => {
+  await ConnectDB();
+};
+
+LoadDB();
 
 export async function GET(request) {
   return NextResponse.json({ message: "get method hit" });
