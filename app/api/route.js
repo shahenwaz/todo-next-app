45ee1bfaa0f2d302316875a8a6c95a8630cb98ed
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-06-26 02:16:52 +0100

    POST method added to API route

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,5 @@
 import { ConnectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/models/TodoModel";
 import { NextResponse } from "next/server";
 
 // Database connection
@@ -8,6 +9,19 @@ const LoadDB = async () => {
 
 LoadDB();
 
+// GET method
 export async function GET(request) {
   return NextResponse.json({ message: "get method hit" });
+}
+
+// POST method
+export async function POST(request) {
+  const { title, description } = await request.json();
+
+  await TodoModel.create({
+    title,
+    description,
+  });
+
+  return NextResponse.json({ message: "Todo Created" });
 }
